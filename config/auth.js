39f31ed8b1fcf03d44333268fd51730b4e4c55c9
Siva Boyapati <siva.boyapati@autodesk.com>
2016-06-12 22:53:21 -0700

    Crear vanilla authentication app using passport module

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // facebookAuth: {
+  //   clientID: '896605267054808',
+  //   clientSecret: '28f6a7d2842d5f9ba19e449131694902',
+  //   callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  // },
+  facebookAuth: {
+    clientID: '1729225200699121',
+    clientSecret: '39710af415bd0c6456f2afd14377726a',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
+    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
+    clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
